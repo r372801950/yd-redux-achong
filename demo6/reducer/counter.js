f5cb33d0113c63582526b949be542c0fa271ee6c
jiang <372801950@qq.com>
2025-04-28 08:11:34 +0800

    demo6 init

---
+++ demo6/reducer/counter.js
@@ -0,0 +1,25 @@
+let initState ={
+  count: 0,
+}
+export default function countReducer(state, action) {
+  if (!state) {
+    state = initState;
+  }
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case 'DECREMENT':
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
+// 函数式编程里面
+//   map是接收变形关系
+//   reducer也是接受变形关系
